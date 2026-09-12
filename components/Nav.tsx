f0dc9ad28e908affab2a8d348fae711b77849ba8
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-09-12 20:53:48 +0100

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,8 @@
 export default function Nav() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-[100] flex items-center justify-between px-[5vw] py-7">
+    <nav className="fixed top-0 left-0 right-0 z-[100] flex items-center px-[5vw] py-7">
       <div className="font-display text-[2.2rem] tracking-wide text-black">
         DAVE
-      </div>
-      <div className="text-[0.68rem] tracking-[0.28em] uppercase text-black/70">
-        12th September
       </div>
     </nav>
   );
