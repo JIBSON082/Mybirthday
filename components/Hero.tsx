0e15197443ab1b3838101b91ad3ae3eefa828c7b
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-09-13 19:37:34 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,8 +19,8 @@ function useTypewriterLoop(fullText: string) {
     let i = 0;
     let deleting = false;
 
-    const TYPE_SPEED = 900;
-    const DELETE_SPEED = 560;
+    const TYPE_SPEED = 180;
+    const DELETE_SPEED = 90;
     const HOLD_FULL = 3200;
     const HOLD_EMPTY = 1400;
 
